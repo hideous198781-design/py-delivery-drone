d8d955a1a51e3df60a9d05cd8b36f8c08739c746
Hideous <hideous198781@gmail.com>
2025-12-10 11:33:05 +0200

    another solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional
 
 
 class Cargo:
@@ -34,11 +34,13 @@ class FlyingRobot(BaseRobot):
         name: str,
         weight: int,
         coords: list[int] | None = None,
-        altitude: int | None = None,
+        altitude: int | None = 0,
     ) -> None:
+        # Використовуємо коротке присвоєння coords і altitude
         super().__init__(name, weight, coords)
         self.altitude = altitude or 0
 
+        # Забезпечуємо coords у форматі [x, y, z]
         if len(self.coords) == 2:
             self.coords.append(self.altitude)
 
@@ -55,29 +57,28 @@ class DeliveryDrone(FlyingRobot):
         name: str,
         weight: int,
         max_load_weight: int,
-        current_load=None,
+        current_load: Optional[Cargo] = None,
         coords: list[int] | None = None,
-        altitude: int | None = None,
+        altitude: int | None = 0,
     ) -> None:
         super().__init__(name, weight, coords, altitude)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
-    def hook_load(self, load_weight) -> str:
-        # load_weight — це Cargo
+    def hook_load(self, cargo: Cargo) -> str:
         if self.current_load is not None:
             return "Already have load"
 
-        weight = load_weight.weight
+        weight = cargo.weight
 
         if weight > self.max_load_weight:
             return "Load is too heavy"
 
         if weight == self.max_load_weight:
-            self.current_load = load_weight
+            self.current_load = cargo
             return "Load equals max weight — be careful!"
 
-        self.current_load = load_weight
+        self.current_load = cargo
         return "Load hooked"
 
     def unhook_load(self) -> str:
